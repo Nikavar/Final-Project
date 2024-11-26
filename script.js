04298e4a9c650ffe0c12d087341d6e9c7503c644
varamashvili.nikoloz <varamashvili.nikoloz@gmail.com>
2024-11-26 15:49:26 +0400

    finished fst page

---
+++ script.js
@@ -0,0 +1,30 @@
+// on | off burger / xMark visibility
+let burger = document.querySelector(".header_burger_button");
+let xMark = document.querySelector(".header_x_mark");
+let mobileMenu = document.querySelector(".header_nav");
+
+burger.addEventListener("click",() => {
+    mobileMenu.style.left = "0px";
+    xMark.style.display = "block";
+    burger.style.display = "none";
+});
+
+xMark.addEventListener("click",()=>{
+    mobileMenu.style.left = "-100%";
+    xMark.style.display = "none";
+    burger.style.display = "block";
+})
+
+
+// set a correct color: #D5906E for active page tab 
+const navLinks = document.querySelectorAll('.header_li_a');
+
+const currentPage = window.location.pathname;
+
+navLinks.forEach(link => {
+    if (link.pathname === currentPage) {
+        link.classList.add('active'); 
+    } else {
+        link.classList.remove('active'); 
+    }
+});
